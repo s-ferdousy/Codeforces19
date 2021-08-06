b1261728d7e15b9b3862e354c1bb8d2051591343
s-ferdousy <lopa.ferdousy@gmail.com>
2021-08-06 13:17:40 +0600

    Create 52A - 123-sequence.py

---
+++ 52A - 123-sequence.py
@@ -0,0 +1,16 @@
+n=int(input())
+a=list(map(int,input().split()))
+
+uno=0
+dos=0
+tres=0
+
+for i in range(n):
+    if a[i]==1:
+        uno+=1
+    elif a[i]==2:
+        dos+=1
+    else:
+        tres+=1
+
+print(uno+dos+tres-max(uno,dos,tres))
